5e2aa98b2f109464e59463d7f86d31ffba4b5511
yHenrique <henriquey08@gmail.com>
2026-03-06 01:18:26 -0300

    Organizando pastas e arquivos

--- src/etl.py
+++ src/etl.py
@@ -1,8 +1,12 @@
 import pandas as pd
+# import country_converter as coco
+import deep_translator as dt
 
-df = pd.read_csv('students_social_media_addiction.csv')
+# Importando a base de dados
+df = pd.read_csv('data/base_original.csv')
 
-df_preparacao = df.rename(columns={'Student_ID':'ID',
+# Traduzindo os nomes das colunas
+df_tratado = df.rename(columns={'Student_ID':'ID',
                                 'Age':'Idade',
                                 'Gender':'Gênero',
                                 'Academic_Level':'Nível_Acadêmico',
@@ -19,30 +23,36 @@ df_preparacao = df.rename(columns={'Student_ID':'ID',
 # Traduzindo a coluna Gênero
 traduzir_genero = {'Female':'Feminino',
                    'Male':'Masculino'}
-df_preparacao['Gênero'] = df_preparacao['Gênero'].map(traduzir_genero)
+df_tratado['Gênero'] = df_tratado['Gênero'].map(traduzir_genero)
 
 # Traduzindo a coluna Nível_Acadêmico
 traduzir_nivel_academico = {'Undergraduate':'Graduação',
                             'Graduate':'Pós-Graduação',
                             'High School':'Ensino Médio'}
-df_preparacao['Nível_Acadêmico'] = df_preparacao['Nível_Acadêmico'].map(traduzir_nivel_academico)
+df_tratado['Nível_Acadêmico'] = df_tratado['Nível_Acadêmico'].map(traduzir_nivel_academico)
 
 # Traduzindo a coluna País
-# Utilizar biblioteca
+# print(df_tratado['País'].unique())
+df_tratado['País'] = df_tratado['País'].replace({'USA':'United States', 'UK':'United Kingdom', 'UAE':'United Arab Emirates'})
+# for pais in df_tratado['País']:
+#     pais_traduzido = dt.GoogleTranslator(source='en', target='pt').translate(pais)
+#     df_tratado['País'][pais] = pais_traduzido
 
 # Traduzindo a coluna Afeta_Desempenho_Acadêmico
 traduzir_afeta_desempenho_academico = {'Yes':'Sim',
                                        'No':'Não'}
-df_preparacao['Afeta_Desempenho_Acadêmico'] = df_preparacao['Afeta_Desempenho_Acadêmico'].map(traduzir_afeta_desempenho_academico)
+df_tratado['Afeta_Desempenho_Acadêmico'] = df_tratado['Afeta_Desempenho_Acadêmico'].map(traduzir_afeta_desempenho_academico)
 
 # Traduzindo a coluna Status_Relação
 traduzir_status_relacao = {'In Relationship':'Em Relacionamento',
                            'Single':'Solteiro',
                            'Complicated':'Complicado'}
-df_preparacao['Status_Relação'] = df_preparacao['Status_Relação'].map(traduzir_status_relacao)
+df_tratado['Status_Relação'] = df_tratado['Status_Relação'].map(traduzir_status_relacao)
 
+# Criando a base tratada
+df_tratado.to_csv('data/base_tratada.csv', index=False)
 
-
-# print(df_preparacao['Status_Relação'].unique())
-# df_preparacao.info()
-# print(df_preparacao.head())
+# Exibindo informações
+# print(df_tratado['Status_Relação'].unique())
+# df_tratado.info()
+# print(df_tratado.head())
